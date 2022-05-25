f196237b8b2dcea7a30b766d55dae07081a1c639
Juan Arano <juanarano17@gmail.com>
2022-05-25 14:34:51 -0300

    seteando las bases de la app

--- AppMiPedicenter.py
+++ AppMiPedicenter.py
@@ -6,9 +6,33 @@ app = Flask(__name__)
 def home():
         return render_template('home.html')
 
-@app.route("/about")
-def about():
-        return "About Page"
+@app.route("/login")
+def login():
+        return render_template('login.html', title="LogIn")
+
+@app.route("/podologo")
+def podo():
+        return render_template('podologo.html', title = "Podologo")
+
+@app.route("/administrador")
+def admin():
+        return render_template('administrdor.html', title="Centro de accion del Administrador")
+
+@app.route("/recepcionista")
+def recep():
+        return render_template('recepcionista.html', title="Centro de accion del Recepcionista")
+
+@app.route("/recepcionista/calendario")
+def calen_recep():
+        return render_template('calen_recep.html', title="Calendario Vista Recepcionista")
+
+@app.route("/podologo/calendario")
+def calen_podo():
+        return render_template('calen_podo.html', title="Calendario Vista Podologo")
+
+@app.route("/podologo/historia-clinica")
+def historiaClinica():
+        return render_template('hist_clin.html', title="Historias Clinicas")
 
 if __name__ == '__main__':
-    app.run(debug=True)
+        app.run(debug=True)
