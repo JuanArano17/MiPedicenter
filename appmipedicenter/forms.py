e0e1962c2ae45b9bbbd9c3b97617bbf5b5f9a931
Juan Arano <juanarano17@gmail.com>
2022-07-09 11:32:26 -0300

    Revision de funcionalidades y correccion de errores relacionados a la base de datos.

--- appmipedicenter/forms.py
+++ appmipedicenter/forms.py
@@ -31,7 +31,6 @@ class RegistrationForm(FlaskForm):
 class LoginForm(FlaskForm):
     email = StringField('E-Mail', validators=[DataRequired(), Email()])
     password = PasswordField('Contraseña', validators=[DataRequired()])
-    remember = BooleanField('Remember Me')
     submit = SubmitField('Acceder')
 
 
@@ -40,14 +39,7 @@ class UpdateAccountForm(FlaskForm):
     dni = IntegerField('DNI', validators=[DataRequired()])
     fecha_nacimiento = DateField('Fecha Nacimiento YYYY-MM-DD', validators=[DataRequired()])
     telefono = IntegerField('Nro de Telefono (solo carácteres numéricos)', validators=[DataRequired()])
-    email = StringField('E-Mail', validators=[DataRequired(), Email()])
     submit = SubmitField('Actualizar')
-
-    def validate_email(self, email):
-        if email.data != current_user.id:
-            empleado = Empleado.query.filter_by(id= email.data).first()
-            if empleado:
-                raise ValidationError("El email ya esta en uso")
 
     def validate_dni(self, dni):
         if dni.data != current_user.dni:
@@ -73,6 +65,11 @@ class ClienteForm(FlaskForm):
         if cliente:
             raise ValidationError("El Email ya esta en uso")
 
+    def validate_telefono(self, telefono):
+        cliente = Cliente.query.filter_by(telefono = telefono.data).first()
+        if cliente:
+            raise ValidationError("El telefono ya esta en uso")
+
 class ClienteUpdateForm(FlaskForm):
     dni = IntegerField('DNI', validators=[DataRequired()])
     username = StringField('Nombre Completo', validators=[DataRequired()])
@@ -82,10 +79,20 @@ class ClienteUpdateForm(FlaskForm):
     submit = SubmitField('Modificar')
 
 class AsignarTurno(FlaskForm):
-    dni = IntegerField('DNI', validators=[DataRequired()])
-    ivalor = 0
-    jvalor = 0
-    submit = SubmitField('Guardar Turno')
+    dni = IntegerField('DNI paciente', validators=[DataRequired()])
+    id_hora = SelectField(u'Hora de Turno', choices=[('1', '10:00'), ('2', '10:30'), ('3', '11:00')
+                                                    , ('4', '11:30'), ('5', '12:00'), ('6', '12:30')
+                                                    , ('7', '13:00'), ('8', '13:30'), ('9', '14:00')
+                                                    , ('10', '14:30'), ('11', '15:00'), ('12', '15:30')
+                                                    , ('13', '16:00'), ('14', '16:30'), ('15', '17:00')
+                                                    , ('16', '17:30'), ('17', '18:00'), ('18', '18:30')])
+    listaPodologo = Empleado.query.filter_by(id_tipo = 1).all()
+    listaPodologoSelect = []
+    for i in range(0, len(listaPodologo)):
+        listaPodologoSelect.append((listaPodologo[i].id, listaPodologo[i].username))
+    id_podologo = SelectField(u'Seleccione Podologo', choices=listaPodologoSelect)
+    date = DateField('Fecha', validators=[DataRequired()])
+    submitUpdate = SubmitField('Guardar Turno')
 
     def validate_dni(self, dni):
         cliente = Cliente.query.filter_by(id_cliente = dni.data).first()
--- appmipedicenter/routes.py
+++ appmipedicenter/routes.py
@@ -4,9 +4,10 @@ from flask import render_template, url_for, flash, redirect, request
 from appmipedicenter.models import *
 from appmipedicenter.forms import RegistrationForm, LoginForm, UpdateAccountForm, ClienteForm, ClienteUpdateForm, AsignarTurno
 from appmipedicenter.Plantilla import Plantilla
-from appmipedicenter import app, bcrypt, db
+from appmipedicenter import app, bcrypt, db, mail
 from flask_login import login_user, current_user, logout_user, login_required
 from datetime import *
+from flask_mail import Message
 
 @app.route("/")
 @app.route("/home")
@@ -41,7 +42,7 @@ def login():
         if form.validate_on_submit():
                 empleado = Empleado.query.filter_by(id = form.email.data).first()
                 if empleado and bcrypt.check_password_hash(empleado.password, form.password.data):
-                        login_user(empleado, remember=form.remember)
+                        login_user(empleado, remember=True)
                         next_page = request.args.get('next')
                         flash(f'{empleado.username}, has sido logeado con exito', 'success')
                         return redirect(next_page) if next_page else redirect (url_for('home'))
@@ -57,11 +58,11 @@ def logout():
 @app.route("/mi-perfil", methods=['GET','POST'])
 @login_required
 def mi_perfil():
+        cuenta_tipo = Tipoempleado.query.filter_by(id_tipo = current_user.id_tipo).first()
         form = UpdateAccountForm()
         if form.validate_on_submit():
                 current_user.username= form.username.data
                 current_user.dni= form.dni.data
-                current_user.id= form.email.data
                 current_user.telefono= form.telefono.data
                 current_user.fecha_nacimiento= form.fecha_nacimiento.data
                 db.session.commit()
@@ -70,10 +71,28 @@ def mi_perfil():
         elif request.method=='GET':
                 form.username.data = current_user.username
                 form.dni.data = current_user.dni
-                form.email.data = current_user.id
                 form.telefono.data = current_user.telefono
                 form.fecha_nacimiento.data = current_user.fecha_nacimiento 
-        return render_template('mi_perfil.html', title="MiPerfil", form=form)
+        return render_template('mi_perfil.html', title="MiPerfil", form=form, cuenta_tipo = cuenta_tipo)
+
+
+@app.route("/eliminar-cuenta/<string:id>", methods=['POST'])
+@login_required
+def eliminar_cuenta(id):
+        empleado = Empleado.query.get_or_404(id)
+        if empleado != current_user:
+                abort(403)
+        listaTurnosEmpleado = Turno.query.filter_by(id_empleado = empleado.id)
+        for i in listaTurnosEmpleado:
+                db.session.delete(i)
+        db.session.delete(empleado)
+        db.session.commit()
+        flash('Tu cuenta ha sido eliminada', 'success')
+        logout_user()
+        return redirect(url_for('home'))
+        
+
+
 
 @app.route("/mi-calendario") #Incluye CRUD Turnos (envio mail), CURD Clientes y Disp Horario
 @login_required
@@ -86,23 +105,27 @@ def mi_calendario():
 @app.route("/plantilla-turnos/<string:date_str>", methods=['POST', 'GET'])
 @login_required
 def plantilla_turnos(date_str):
-        datePlant = date.fromisoformat(date_str)
-        plantilla = Plantilla(datePlant)
-        if (current_user.id_tipo == 1):     #Incluye CRUD Turnos, Disp Horario e Historias Clinicas
-                return render_template('plantilla_podologo.html', title="Calendario Vista Podologo", plantilla=plantilla)
-        elif (current_user.id_tipo == 2):
-                form_crear_turno = AsignarTurno()
-                if form_crear_turno.validate_on_submit():
-                        turno = Turno.query.filter_by(id_hora = form_crear_turno.ivalor , id_empleado = plantilla[0][form_crear_turno.jvalor], date = datePlant).first()
-                        turno.id_cliente = form_crear_turno.dni.data
-                        db.session.commit()
-                        flash('Se ha asignado el turno con exito', 'success')
-                        return redirect(url_for("plantilla_turnos", date_str = date_str))
-                return render_template('plantilla_recep.html', title="Calendario Vista Recepcionista", plantilla=plantilla, form_crear_turno=form_crear_turno) 
+        if current_user.is_authenticated:
+                datePlant = date.fromisoformat(date_str)
+                plantilla = Plantilla(datePlant)
+                if (current_user.id_tipo == 1):     #Incluye CRUD Turnos, Disp Horario e Historias Clinicas
+                        return render_template('plantilla_podologo.html', title="Calendario Vista Podologo", plantilla=plantilla)
+                elif (current_user.id_tipo == 2):
+                        form_crear_turno = AsignarTurno()
+                        if form_crear_turno.validate_on_submit():
+                                form_crear_turno.date = datePlant
+                                flash('Se ha asignado el turno con exito', 'success')
+                                turno = Turno.query.filter_by(date = form_crear_turno.date, id_empleado = form_crear_turno.id_podologo.data, id_hora = form_crear_turno.id_hora.data).first()
+                                turno.id_cliente = form_crear_turno.dni.data
+                                db.session.commit()
+                                return redirect(url_for("plantilla_turnos", date_str = date_str))
+                        return render_template('plantilla_recep.html', title="Calendario Vista Recepcionista", plantilla=plantilla, form_crear_turno=form_crear_turno)
+        else:
+                return redirect(url_for('home'))
 
-@app.route("/clientes", methods=['GET', 'POST'])
+@app.route("/pacientes", methods=['GET', 'POST'])
 @login_required
-def clientes():
+def pacientes():
         if current_user.is_authenticated:
                 if current_user.id_tipo == 2: 
                         client_list = Cliente.query.all()
@@ -118,52 +141,64 @@ def clientes():
                                 db.session.add(cliente)
                                 db.session.commit()
                                 flash(f'El cliente {form.username.data} ha sido ingresado/a con exito.', 'success')
-                                return redirect(url_for('clientes'))
-                        return render_template('clientes.html', title="Gestion de Pacientes", clientes = client_list, form=form) 
+                                return redirect(url_for('pacientes'))
+                        return render_template('pacientes.html', title="Gestion de Pacientes", clientes = client_list, form=form) 
                 else:
                         return redirect(url_for('home'))
         else:
                 return redirect(url_for('login'))
 
-@app.route("/clientes/editar/<int:id_cliente>", methods=['GET', 'POST'])
+@app.route("/pacientes/editar/<int:id_cliente>", methods=['GET', 'POST'])
 @login_required
-def editar_clientes(id_cliente):
+def editar_pacientes(id_cliente):
         if current_user.is_authenticated:
                 if current_user.id_tipo == 2: 
-                        client = Cliente.query.filter_by(id_cliente = id_cliente).first()
+                        cliente = Cliente.query.filter_by(id_cliente = id_cliente).first()
                         form = ClienteUpdateForm()
                         if form.validate_on_submit():
-                                client.id_cliente = form.dni.data
-                                client.username = form.username.data
-                                client.email = form.email.data
-                                client.fecha_nacimiento = form.fecha_nacimiento.data
-                                client.telefono = form.telefono.data
+                                if cliente.id_cliente != form.dni.data:
+                                        listaTurnosCliente = Turno.query.filter_by(id_cliente = cliente.id_cliente)
+                                        for turno in listaTurnosCliente:
+                                                turno.id_cliente = form.dni.data
+                                cliente.id_cliente = form.dni.data
+                                cliente.username = form.username.data
+                                cliente.email = form.email.data
+                                cliente.fecha_nacimiento = form.fecha_nacimiento.data
+                                cliente.telefono = form.telefono.data
                                 db.session.commit()
                                 flash(f'Los datos de {form.username.data} han sido actualizados.', 'success')
-                                return redirect(url_for('clientes'))
+                                return redirect(url_for('pacientes'))
                         elif request.method=='GET':
-                                form.username.data = client.username
-                                form.dni.data = client.id_cliente
-                                form.email.data = client.email
-                                form.telefono.data = client.telefono
-                                form.fecha_nacimiento.data = client.fecha_nacimiento 
-                        return render_template('editar_cliente.html', title="Edicion de Clientes", form=form)
+                                form.username.data = cliente.username
+                                form.dni.data = cliente.id_cliente
+                                form.email.data = cliente.email
+                                form.telefono.data = cliente.telefono
+                                form.fecha_nacimiento.data = cliente.fecha_nacimiento 
+                        return render_template('editar_paciente.html', title="Edicion de Clientes", form=form)
                 else:
                         return redirect(url_for('home'))
         else:
                 return redirect(url_for('login'))
                 
-@app.route("/clientes/eliminar/<int:id_cliente>", methods=['GET', 'POST'])
+@app.route("/pacientes/eliminar/<int:id_cliente>", methods=['GET', 'POST'])
 @login_required
-def eliminar_clientes(id_cliente):
+def eliminar_pacientes(id_cliente):
         if current_user.is_authenticated:
                 if current_user.id_tipo == 2: 
                         cliente = Cliente.query.filter_by(id_cliente = id_cliente).first()
-                        Cliente.query.filter_by(id_cliente = id_cliente).delete()
+                        lista_turnos_cliente = Turno.query.filter_by(id_cliente = cliente.id_cliente)
+                        for i in lista_turnos_cliente:
+                                db.session.delete(i)
+                        db.session.delete(cliente)
                         db.session.commit()
                         flash(f'Los datos de {cliente.username} han sido borrados.', 'success')
-                        return redirect(url_for('clientes'))
+                        return redirect(url_for('pacientes'))
                 else:
                         return redirect(url_for('home'))
         else:
                 return redirect(url_for('login'))
+
+def mandar_mail_turno(Cliente, Date):
+        msg = Message('Confirmacion de turno Pedicenter', sender='mipedicenter@gmail.com', recipients=[Cliente.email])
+        msg.body = f''' Un turno podologico ha sido asignado exitosamente para el dia {Date} en la sede Arenales 1283 PB.'''
+        mail.send(msg)
