d6cbe2d09dea7a7ebab86ac4abd8a2e560f43f9d
Juan Arano <juanarano17@gmail.com>
2022-07-11 17:50:48 -0300

    Agregando partes encesarias para el funcionamiento de la app en Heroku

--- appmipedicenter/config.py
+++ appmipedicenter/config.py
@@ -1,6 +1,7 @@
 class Config:
     SECRET_KEY = '4ecd725281820152a8d856062cbc77'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    #SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db' 
+    SQLALCHEMY_DATABASE_URI = 'postgres://zonmaomrqubdfk:d84eceb66361cd9ee37c9d80fe1874bc6a3201dc3328c734cae8ae5b19aba0fa@ec2-3-222-74-92.compute-1.amazonaws.com:5432/da0et3jea36bcb'
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 465
     MAIL_USERNAME = 'mipedicenter@gmail.com'
