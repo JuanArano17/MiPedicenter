5d969a3f092884b1addb3a8cf17deb9743166229
Juan Pablo Arano <96890735+JuanArano17@users.noreply.github.com>
2022-09-06 15:25:18 -0300

    crud Historias Clinicas generales

--- appmipedicenter/historiaclinica/forms.py
+++ appmipedicenter/historiaclinica/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, BooleanField, TextAreaField
-from wtforms.validators import DataRequired
+from wtforms import StringField, SubmitField, BooleanField, TextAreaField, IntegerField
+from wtforms.validators import DataRequired, ValidationError
+from appmipedicenter.models import Cliente
 
 class HistoriaClinicaForm(FlaskForm):
     diagnostico = TextAreaField('Diagnostico General', validators=[DataRequired()])
@@ -9,3 +10,19 @@ class HistoriaClinicaForm(FlaskForm):
     onicopatias = StringField('Onicopatias')
     otros_datos = StringField('Otros datos')
     submit = SubmitField('Guardar')
+
+class TablaHistoriaClinicaForm(FlaskForm):
+    diagnostico = TextAreaField('Diagnostico General', validators=[DataRequired()])
+    diabetes = BooleanField('¿Posee diabetes?')
+    antecedentes = StringField('Antecedentes')
+    onicopatias = StringField('Onicopatias')
+    otros_datos = StringField('Otros datos')
+    id_cliente = IntegerField('DNI paciente',validators=[DataRequired()])
+    submit = SubmitField('Guardar')
+
+    def validate_id_cliente(self, id_cliente):
+        cliente = Cliente.query.filter_by(id_cliente = id_cliente.data).first()
+        if not bool(cliente):
+            raise ValidationError("El cliente no esta registrado.")
+
+
--- appmipedicenter/historiaclinica/routes.py
+++ appmipedicenter/historiaclinica/routes.py
@@ -1,7 +1,7 @@
 from http.client import REQUEST_HEADER_FIELDS_TOO_LARGE
 from flask import render_template, url_for, flash, redirect, request, Blueprint
 from appmipedicenter.models import Empleado, Historiaclinica, Cliente
-from appmipedicenter.historiaclinica.forms import HistoriaClinicaForm, HistoriaClinicaForm
+from appmipedicenter.historiaclinica.forms import HistoriaClinicaForm, HistoriaClinicaForm, TablaHistoriaClinicaForm
 from appmipedicenter import db
 from flask_login import current_user, login_required
 from datetime import *
@@ -37,7 +37,7 @@ def historias_clinicas(id_cliente):
 
 @historiaclinica.route("/historias-clinicas/eliminar/<int:id_historia>", methods=['GET', 'POST'])
 @login_required
-def eliminar_historia(id_historia):
+def eliminar_historia(id_historia, num):
         if current_user.is_authenticated:
                 if current_user.id_tipo == 1: 
                         historia = Historiaclinica.query.filter_by(id_historia_clinica = id_historia).first()
@@ -45,7 +45,10 @@ def eliminar_historia(id_historia):
                         db.session.delete(historia)
                         db.session.commit()
                         flash('La historia clinica ha sido eliminada.', 'success')
-                        return redirect(url_for('historiaclinica.historias_clinicas', id_cliente=id_cliente))
+                        if num==1:
+                                return redirect(url_for('historiaclinica.historias_clinicas', id_cliente=id_cliente))
+                        else:
+                                return redirect(url_for('historiaclinica.tablaHC'))
                 else:
                         return redirect(url_for('main.home'))
         else:
@@ -53,7 +56,7 @@ def eliminar_historia(id_historia):
 
 @historiaclinica.route("/historias-clinicas/editar/<int:id_historia>", methods=['GET', 'POST'])
 @login_required
-def editar_historia(id_historia):
+def editar_historia(id_historia, num):
         if current_user.is_authenticated:
                 if current_user.id_tipo == 1: 
                         historia = Historiaclinica.query.filter_by(id_historia_clinica = id_historia).first()
@@ -67,7 +70,10 @@ def editar_historia(id_historia):
                                 historia.otros_datos = form.otros_datos.data
                                 db.session.commit()
                                 flash('Los datos de la historia clinica han sido actualizados.', 'success')
-                                return redirect(url_for('historiaclinica.historias_clinicas', id_cliente=id_cliente))
+                                if num==1:
+                                        return redirect(url_for('historiaclinica.historias_clinicas', id_cliente=id_cliente))
+                                else:
+                                        return redirect(url_for('historiaclinica.tablaHC'))
                         elif request.method=='GET':
                                 form.diagnostico.data =  historia.diagnostico
                                 form.diabetes.data = historia.diabetes
@@ -79,3 +85,35 @@ def editar_historia(id_historia):
                         return redirect(url_for('main.home'))
         else:
                 return redirect(url_for('empleado.login'))
+
+####SOFIA###
+@historiaclinica.route("/tabla_historias_clinicas", methods=['GET', 'POST'])
+@login_required
+def tablaHC():
+        if current_user.is_authenticated and current_user.id_tipo == 1:
+                historiasclinicas = Historiaclinica.query.all()
+                clientes = Cliente.query.all()
+                podologos = Empleado.query.all()
+                form = TablaHistoriaClinicaForm() 
+                if form.validate_on_submit():
+                        hc = Historiaclinica(
+                                datetime = datetime.now(),
+                                diagnostico = form.diagnostico.data,  
+                                diabetes = form.diabetes.data,
+                                antecedentes = form.antecedentes.data,
+                                onicopatias = form.onicopatias.data,
+                                otros_datos = form.otros_datos.data,
+                                id_empleado = current_user.id,                                        
+                                id_cliente = form.id_cliente.data
+                        )
+                        db.session.add(hc)
+                        db.session.commit()
+                        flash(f'La Historia Clinica ha sido creada con exito.', 'success')
+                        return redirect(url_for('historiaclinica.tablaHC'))
+                return render_template('tabla_historias_clinicas.html', title="Gestion de Historias Clinicas", historiasclinicas = historiasclinicas, podologos = podologos, clientes = clientes, form=form) 
+        else:
+                return redirect(url_for('empleado.login'))
+
+
+
+
